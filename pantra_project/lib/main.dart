e17246f7df96ae29c30a340d42153db0aeced81a
Handrian Alandi <handrianalandi@gmail.com>
2022-06-15 12:23:27 +0700

    merging

--- pantra_project/lib/main.dart
+++ pantra_project/lib/main.dart
@@ -1,30 +1,163 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:pantra_project/page/login.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MaterialApp(home: MyApp(), theme: ThemeData(fontFamily: 'Recoleta')));
 }
 
 class MyApp extends StatefulWidget {
-  const MyApp({ Key? key }) : super(key: key);
+  const MyApp({Key? key}) : super(key: key);
 
   @override
   State<MyApp> createState() => _MyAppState();
 }
 
-class _MyAppState extends State<MyApp> {
+class _MyAppState extends State<MyApp> with SingleTickerProviderStateMixin {
+  late AnimationController _contrtoller;
+  startTimer() async {
+    Timer(const Duration(seconds: 4), nextScreen);
+  }
+
+  void nextScreen() {
+    Navigator.push(context, MaterialPageRoute(builder: (context) => Login()));
+  }
+
+  @override
+  void initState() {
+    _contrtoller = AnimationController(
+      vsync: this,
+      duration: const Duration(seconds: 4),
+    );
+    _contrtoller.forward();
+    startTimer();
+    super.initState();
+  }
+
+  late final Animation<Offset> _offsetAnimation = Tween<Offset>(
+    begin: const Offset(-5, 0.0),
+    end: Offset.zero,
+  ).animate(CurvedAnimation(
+    parent: _contrtoller,
+    curve: Interval(0.0, 0.5, curve: Curves.elasticOut),
+  ));
+  late final Animation<Offset> _offsetAnimation2 = Tween<Offset>(
+    begin: const Offset(-6, 0.0),
+    end: Offset.zero,
+  ).animate(CurvedAnimation(
+    parent: _contrtoller,
+    curve: Interval(0.0, 0.6, curve: Curves.elasticOut),
+  ));
+  late final Animation<Offset> _offsetAnimation3 = Tween<Offset>(
+    begin: const Offset(-7, 0.0),
+    end: Offset.zero,
+  ).animate(CurvedAnimation(
+    parent: _contrtoller,
+    curve: Interval(0.0, 0.7, curve: Curves.elasticOut),
+  ));
+  late final Animation<Offset> _offsetAnimation4 = Tween<Offset>(
+    begin: const Offset(-8, 0.0),
+    end: Offset.zero,
+  ).animate(CurvedAnimation(
+    parent: _contrtoller,
+    curve: Interval(0.0, 0.8, curve: Curves.elasticOut),
+  ));
+  late final Animation<Offset> _offsetAnimation5 = Tween<Offset>(
+    begin: const Offset(-9, 0.0),
+    end: Offset.zero,
+  ).animate(CurvedAnimation(
+    parent: _contrtoller,
+    curve: Interval(0.0, 0.9, curve: Curves.elasticOut),
+  ));
+  late final Animation<Offset> _offsetAnimation6 = Tween<Offset>(
+    begin: const Offset(-10, 0.0),
+    end: Offset.zero,
+  ).animate(CurvedAnimation(
+    parent: _contrtoller,
+    curve: Interval(0.0, 1.0, curve: Curves.elasticOut),
+  ));
+
+  @override
+  void dispose() {
+    _contrtoller.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: "Pantra",
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text("Pantra"),
+    return Scaffold(
+      backgroundColor: Color.fromRGBO(249, 234, 213, 1),
+      body: Center(
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            // SlideTransition(
+            //   position: _offsetAnimation,
+            //   child: SvgPicture.asset("svg/star.svg",
+            //       height: 50,
+            //       width: 50,
+            //       color: Color.fromRGBO(60, 108, 180, 1)),
+            // ),
+            SlideTransition(
+              position: _offsetAnimation,
+              child: const Text("P",
+                  style: TextStyle(
+                      fontSize: 50,
+                      color: Color.fromRGBO(60, 108, 180, 1),
+                      fontFamily: 'Recoleta')),
+            ),
+            SlideTransition(
+              position: _offsetAnimation2,
+              child: const Text("A",
+                  style: TextStyle(
+                      fontSize: 50,
+                      color: Color.fromRGBO(60, 108, 180, 1),
+                      fontFamily: 'Recoleta')),
+            ),
+            SlideTransition(
+              position: _offsetAnimation3,
+              child: const Text("N",
+                  style: TextStyle(
+                      fontSize: 50,
+                      color: Color.fromRGBO(60, 108, 180, 1),
+                      fontFamily: 'Recoleta')),
+            ),
+            SlideTransition(
+              position: _offsetAnimation4,
+              child: const Text("T",
+                  style: TextStyle(
+                      fontSize: 50,
+                      color: Color.fromRGBO(60, 108, 180, 1),
+                      fontFamily: 'Recoleta')),
+            ),
+            SlideTransition(
+              position: _offsetAnimation5,
+              child: const Text("R",
+                  style: TextStyle(
+                      fontSize: 50,
+                      color: Color.fromRGBO(60, 108, 180, 1),
+                      fontFamily: 'Recoleta')),
+            ),
+            SlideTransition(
+              position: _offsetAnimation6,
+              child: const Text("A",
+                  style: TextStyle(
+                      fontSize: 50,
+                      color: Color.fromRGBO(60, 108, 180, 1),
+                      fontFamily: 'Recoleta')),
+            ),
+            // SlideTransition(
+            //   position: _offsetAnimation6,
+            //   child: SvgPicture.asset("svg/star.svg",
+            //       height: 50,
+            //       width: 50,
+            //       color: Color.fromRGBO(60, 108, 180, 1)),
+            // ),
+          ],
         ),
-        body: Container(
-          padding: EdgeInsets.all(10),
-          child: Container(),
-        ),
-      )
+      ),
     );
   }
 }
---
+++ pantra_project/lib/page/login.dart
@@ -0,0 +1,125 @@
+// ignore_for_file: prefer_const_literals_to_create_immutables, prefer_const_constructors
+
+import 'dart:developer';
+
+import 'package:flutter/material.dart';
+
+class Login extends StatefulWidget {
+  const Login({Key? key}) : super(key: key);
+
+  @override
+  State<Login> createState() => _LoginState();
+}
+
+class _LoginState extends State<Login> {
+  final _accountController = TextEditingController();
+  final _passwordController = TextEditingController();
+
+  @override
+  void dispose() {
+    _accountController.dispose();
+    _passwordController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color.fromRGBO(249, 234, 213, 1),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              "Namaste",
+              style: TextStyle(
+                  fontSize: 30,
+                  color: Color.fromRGBO(60, 108, 180, 1),
+                  fontFamily: 'Recoleta'),
+            ),
+            SizedBox(
+              height: 8,
+            ),
+            Container(
+              width: 300,
+              child: TextField(
+                controller: _accountController,
+                decoration: InputDecoration(
+                  filled: true,
+                  prefixIcon: Icon(Icons.person),
+                  labelText: "NRP",
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(30),
+                    borderSide: BorderSide.none,
+                    //make the border transparent
+                  ),
+                  fillColor: Color.fromRGBO(253, 205, 95, 1),
+                  focusedBorder: OutlineInputBorder(
+                    borderSide: const BorderSide(
+                        color: Color.fromRGBO(253, 126, 20, 1), width: 2.0),
+                    borderRadius: BorderRadius.circular(30.0),
+                  ),
+                ),
+              ),
+            ),
+            SizedBox(
+              height: 8,
+            ),
+            Container(
+              width: 300,
+              child: TextField(
+                obscureText: true,
+                controller: _passwordController,
+                decoration: InputDecoration(
+                  filled: true,
+                  prefixIcon: Icon(Icons.lock),
+                  labelText: "Password",
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(30),
+                    borderSide: BorderSide.none,
+                    //make the border transparent
+                  ),
+                  fillColor: Color.fromRGBO(253, 205, 95, 1),
+                  focusedBorder: OutlineInputBorder(
+                    borderSide: const BorderSide(
+                        color: Color.fromRGBO(253, 126, 20, 1), width: 2.0),
+                    borderRadius: BorderRadius.circular(30.0),
+                  ),
+                ),
+              ),
+            ),
+            SizedBox(
+              height: 8,
+            ),
+            Container(
+              width: 300,
+              height: 50,
+              child: ElevatedButton(
+                onPressed: () {
+                  log("username: ${_accountController.text}");
+                  log("password: ${_passwordController.text}");
+                },
+                child: Text(
+                  "LOGIN",
+                  style: TextStyle(
+                      fontSize: 30,
+                      color: Colors.white,
+                      fontFamily: 'Recoleta'),
+                ),
+                style: ElevatedButton.styleFrom(
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(25),
+                  ),
+                ),
+              ),
+            ),
+            SizedBox(
+              height: 8,
+            ),
+            Text("PANTRA 2022", style: TextStyle(color: Colors.grey)),
+          ],
+        ),
+      ),
+    );
+  }
+}
