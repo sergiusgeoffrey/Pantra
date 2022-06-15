0373ea54e26c2984014340d117967af4f071db28
Handrian Alandi <handrianalandi@gmail.com>
2022-06-15 22:27:12 +0700

    student

--- pantra_project/lib/class/student.dart
+++ pantra_project/lib/class/student.dart
@@ -1,99 +1,34 @@
-class Student {
-  // "id": 1,
-  // "nrp": "c14190033",
-  // "name": "Calvert Tanudihardjo",
-  // "angkatan": 2019,
-  // "jurusan": "Teknologi Industri - Informatika",
-  // "pengalaman": "<i>Whatever you do, work at it with all your heart, as working for the Lord, not for human masters.</i>",
-  // "portfolio": "https://www.linkedin.com/in/calvert-tanudihardjo",
-  // "date_of_birth": "2000-11-23",
-  // "instagram": "vertfrag",
-  // "photo_filepath": "https://bem-internal.petra.ac.id/reach/uploads/cv_photo/c14190033_Calvert Tanudihardjo6awme0yzLd.jpg",
-  // "last_updated": "01-23-VERT 45:67:89"
+class Student{
+  // "nrp": "d11200468",
+  //           "nama": "Timothy Jefferson",
+  //           "fakultas": "Bisnis dan Ekonomi",
+  //           "program": "International Business Management",
+  //           "angkatan": 2020,
+  //           "photo_filepath": "https://bem-internal.petra.ac.id/reach/uploads/cv_photo/d11200468_Timothy JeffersonBlhziP1lX8.jpg"
   String nrp;
-  String name;
-  String angkatan;
-  String jurusan;
-  String pengalaman;
-  String? portfolio;
-  String date_of_birth;
-  String instagram;
+  String nama;
+  String fakultas;
+  String program;
+  int angkatan;
   String photo_filepath;
-  String last_updated;
 
   Student({
     required this.nrp,
-    required this.name,
+    required this.nama,
+    required this.fakultas,
+    required this.program,
     required this.angkatan,
-    required this.jurusan,
-    required this.pengalaman,
-    this.portfolio,
-    required this.date_of_birth,
-    required this.instagram,
     required this.photo_filepath,
-    required this.last_updated,
   });
+
+  factory Student.fromJson(Map<String, dynamic> json) {
+    return Student(
+      nrp: json['nrp'],
+      nama: json['nama'],
+      fakultas: json['fakultas'],
+      program: json['program'],
+      angkatan: json['angkatan'],
+      photo_filepath: json['photo_filepath'],
+    );
+  }
 }
-//make 1 student object
-Student stud = Student(
-  nrp: 'c14190033',
-  name: 'Calvert Tanudihardjo',
-  angkatan: '2019',
-  jurusan: 'Teknologi Industri - Informatika',
-  pengalaman: '<i>Whatever you do, work at it with all your heart, as working for the Lord, not for human masters.</i>',
-  portfolio: "https://www.linkedin.com/in/calvert-tanudihardjo",
-  date_of_birth: '2000-11-23',
-  instagram: 'vertfrag',
-  photo_filepath: 'https://bem-internal.petra.ac.id/reach/uploads/cv_photo/c14190033_Calvert Tanudihardjo6awme0yzLd.jpg',
-  last_updated: '01-23-VERT 45:67:89',
-);
-
-//"id": 62,
-// "nrp": "d11200083",
-// "name": "Kevin Samuel",
-// "angkatan": 2020,
-// "jurusan": "Bisnis dan Ekonomi - Manajemen Bisnis",
-// "pengalaman": "Saya pernah mengikuti kepanitiaan BOM(MLBB)-Spetrakuler, disana saya mendapatkan pengalaman dan relasi.",
-// "date_of_birth": "2002-02-07",
-// "instagram": "kevinsamuel7",
-// "photo_filepath": "https://bem-internal.petra.ac.id/reach/uploads/cv_photo/d11200083_Kevin SamuelzgWe4qGfKS.png",
-// "last_updated": "17-07-2021 17:20:42"
-
-//make 1 student object
-Student stud2 = Student(
-  nrp: 'd11200083',
-  name: 'Kevin Samuel',
-  angkatan: '2020',
-  jurusan:
-  'Bisnis dan Ekonomi - Manajemen Bisnis',
-  pengalaman: 'Saya pernah mengikuti kepanitiaan BOM(MLBB)-Spetrakuler, disana saya mendapatkan pengalaman dan relasi.',
-  date_of_birth: '2002-02-07',
-  instagram: 'kevinsamuel7',
-  photo_filepath: 'https://bem-internal.petra.ac.id/reach/uploads/cv_photo/d11200083_Kevin SamuelzgWe4qGfKS.png',
-  last_updated: '17-07-2021 17:20:42',
-);
-
-// "id": 593,
-//"nrp": "c14200005",
-//"name": "Kevin Gunawan",
-//"angkatan": 2020,
-//"jurusan": "Teknologi Industri - Sistem Informasi Bisnis",
-//"pengalaman": "tidak ada",
-//"date_of_birth": "2001-08-11",
-//"instagram": "kevinz398",
-//"photo_filepath": "https://bem-internal.petra.ac.id/reach/uploads/cv_photo/c14200005_Kevin GunawanWvehaW2Uqp.png",
-//"last_updated": "11-06-2021 15:42:20"
-
-//make 1 student object
-
-Student stud3 = Student(
-  nrp: 'c14200005',
-  name: 'Kevin Gunawan',
-  angkatan: '2020',
-  jurusan: 'Teknologi Industri - Sistem Informasi Bisnis',
-  pengalaman: 'tidak ada',
-  date_of_birth: '2001-08-11',
-  instagram: 'kevinz398',
-  photo_filepath: 'https://bem-internal.petra.ac.id/reach/uploads/cv_photo/c14200005_Kevin GunawanWvehaW2Uqp.png',
-  last_updated: '11-06-2021 15:42:20',
-);
---
+++ pantra_project/lib/services/studentApi.dart
@@ -0,0 +1,49 @@
+import 'dart:developer';
+
+import 'package:flutter/cupertino.dart';
+import 'package:pantra_project/class/event.dart';
+import 'dart:convert';
+import 'package:intl/intl.dart';
+
+import 'package:http/http.dart' as http;
+import 'package:pantra_project/class/student.dart';
+
+class studentService {
+  Future<List<Student>> getAllData() async {
+    DateTime nowJakarta = DateTime.now().toUtc().add(const Duration(hours: 7));
+    String day = DateFormat('dd').format(nowJakarta);
+    String month = DateFormat('MM').format(nowJakarta);
+    int year = nowJakarta.year;
+
+    String token = "RwG${month}ne${year}Kc${day}C9w";
+
+    final response = await http.get(
+        Uri.parse(
+            "https://bem-internal.petra.ac.id/reach/api/student/index.php"),
+        headers: {
+          'Header'
+              'Accept': 'application/json',
+          'Authorization': 'Bearer $token',
+        });
+
+    if (response.statusCode == 200) {
+      final Map jsonData = json.decode(response.body);
+      List<Student> students = [];
+      for (var i = 0; i < jsonData['data'].length; i++) {
+        students.add(
+          Student(
+            nrp: jsonData['data'][i]['nrp'],
+            nama: jsonData['data'][i]['nama'],
+            fakultas: jsonData['data'][i]['fakultas'],
+            program: jsonData['data'][i]['program'],
+            angkatan: jsonData['data'][i]['angkatan'],
+            photo_filepath: jsonData['data'][i]['photo_filepath'],
+          ),
+        );
+      }
+      return students;
+    } else {
+      throw Exception('Failed to load data');
+    }
+  }
+}
