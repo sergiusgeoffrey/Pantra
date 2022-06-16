b1b8d1b5a5d92c0f614e219b843ab3172d24ade2
Handrian Alandi <handrianalandi@gmail.com>
2022-06-16 18:53:36 +0700

    small fix tampilan event detail

--- pantra_project/lib/pages/event_detail.dart
+++ pantra_project/lib/pages/event_detail.dart
@@ -52,8 +52,9 @@ class _eventDetailsState extends State<eventDetails> {
                     "Event Details",
                     textAlign: TextAlign.center,
                     style: TextStyle(
-                      fontSize: MediaQuery.of(context).size.height * 0.05,
+                      fontSize: MediaQuery.of(context).size.height * 0.04,
                       fontWeight: FontWeight.bold,
+                      color: const Color.fromRGBO(60, 108, 180, 1),
                     ),
                   ),
                 ),
@@ -62,199 +63,204 @@ class _eventDetailsState extends State<eventDetails> {
           ),
           SizedBox(
             height: MediaQuery.of(context).size.height * 0.8,
-            child: FutureBuilder<Event>(
-              future: _futureEventDetail,
-              builder: (context, snapshot) {
-                if (snapshot.hasData) {
-                  return Column(children: [
-                    SizedBox(
-                      height: MediaQuery.of(context).size.height * 0.1,
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          Text(
-                            "Event Name",
-                            textAlign: TextAlign.left,
-                            style: TextStyle(
-                              fontSize:
-                                  MediaQuery.of(context).size.height * 0.03,
-                              fontWeight: FontWeight.bold,
-                            ),
-                          ),
-                          Text(
-                            snapshot.data!.name,
-                            textAlign: TextAlign.right,
-                            style: TextStyle(
-                              fontSize:
-                                  MediaQuery.of(context).size.height * 0.03,
-                              fontWeight: FontWeight.bold,
-                            ),
-                          ),
-                        ],
+            child: SingleChildScrollView(
+              child: FutureBuilder<Event>(
+                future: _futureEventDetail,
+                builder: (context, snapshot) {
+                  if (snapshot.hasData) {
+                    return Column(children: [
+                      SizedBox(
+                        height: MediaQuery.of(context).size.height * 0.05,
                       ),
-                    ),
-                    SizedBox(
-                      height: MediaQuery.of(context).size.height * 0.1,
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          Text(
-                            "Event Type",
-                            textAlign: TextAlign.left,
-                            style: TextStyle(
-                              fontSize:
-                                  MediaQuery.of(context).size.height * 0.03,
-                              fontWeight: FontWeight.bold,
+                      SizedBox(
+                          width: MediaQuery.of(context).size.width * 0.70,
+                          height: MediaQuery.of(context).size.height * 0.75,
+                          child: Image.network(
+                            snapshot.data!.posterFilepath,
+                            fit: BoxFit.cover,
+                          )),
+                      SizedBox(
+                        height: MediaQuery.of(context).size.height * 0.1,
+                        child: Column(
+                          children: [
+                            Text(
+                              "Event Name",
+                              textAlign: TextAlign.left,
+                              style: TextStyle(
+                                fontSize:
+                                    MediaQuery.of(context).size.height * 0.03,
+                                fontWeight: FontWeight.bold,
+                              ),
                             ),
-                          ),
-                          Text(
-                            snapshot.data!.type,
-                            textAlign: TextAlign.right,
-                            style: TextStyle(
-                              fontSize:
-                                  MediaQuery.of(context).size.height * 0.03,
-                              fontWeight: FontWeight.bold,
+                            Text(
+                              snapshot.data!.name,
+                              textAlign: TextAlign.right,
+                              style: TextStyle(
+                                fontSize:
+                                    MediaQuery.of(context).size.height * 0.02,
+                                fontWeight: FontWeight.bold,
+                              ),
                             ),
-                          ),
-                        ],
+                          ],
+                        ),
                       ),
-                    ),
-                    Container(
-                      height: MediaQuery.of(context).size.height * 0.1,
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          Text(
-                            "Event Status",
-                            textAlign: TextAlign.left,
-                            style: TextStyle(
-                              fontSize:
-                                  MediaQuery.of(context).size.height * 0.03,
-                              fontWeight: FontWeight.bold,
+                      SizedBox(
+                        height: MediaQuery.of(context).size.height * 0.1,
+                        child: Column(
+                          children: [
+                            Text(
+                              "Event Type",
+                              textAlign: TextAlign.left,
+                              style: TextStyle(
+                                fontSize:
+                                    MediaQuery.of(context).size.height * 0.03,
+                                fontWeight: FontWeight.bold,
+                              ),
                             ),
-                          ),
-                          Text(
-                            snapshot.data!.status,
-                            textAlign: TextAlign.right,
-                            style: TextStyle(
-                              fontSize:
-                                  MediaQuery.of(context).size.height * 0.03,
-                              fontWeight: FontWeight.bold,
+                            Text(
+                              snapshot.data!.type,
+                              textAlign: TextAlign.right,
+                              style: TextStyle(
+                                fontSize:
+                                    MediaQuery.of(context).size.height * 0.02,
+                                fontWeight: FontWeight.bold,
+                              ),
                             ),
-                          ),
-                        ],
+                          ],
+                        ),
                       ),
-                    ),
-                    Container(
-                      height: MediaQuery.of(context).size.height * 0.1,
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          Text(
-                            "Event Date",
-                            textAlign: TextAlign.left,
-                            style: TextStyle(
-                              fontSize:
-                                  MediaQuery.of(context).size.height * 0.03,
-                              fontWeight: FontWeight.bold,
+                      Container(
+                        height: MediaQuery.of(context).size.height * 0.1,
+                        child: Column(
+                          children: [
+                            Text(
+                              "Event Status",
+                              textAlign: TextAlign.left,
+                              style: TextStyle(
+                                fontSize:
+                                    MediaQuery.of(context).size.height * 0.03,
+                                fontWeight: FontWeight.bold,
+                              ),
                             ),
-                          ),
-                          Text(
-                            snapshot.data!.organizer,
-                            textAlign: TextAlign.right,
-                            style: TextStyle(
-                              fontSize:
-                                  MediaQuery.of(context).size.height * 0.03,
-                              fontWeight: FontWeight.bold,
+                            Text(
+                              snapshot.data!.status,
+                              textAlign: TextAlign.right,
+                              style: TextStyle(
+                                fontSize:
+                                    MediaQuery.of(context).size.height * 0.02,
+                                fontWeight: FontWeight.bold,
+                              ),
                             ),
-                          ),
-                        ],
+                          ],
+                        ),
                       ),
-                    ),
-                    Container(
-                      height: MediaQuery.of(context).size.height * 0.1,
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          Text(
-                            "Event Year",
-                            textAlign: TextAlign.left,
-                            style: TextStyle(
-                              fontSize:
-                                  MediaQuery.of(context).size.height * 0.03,
-                              fontWeight: FontWeight.bold,
+                      Container(
+                        height: MediaQuery.of(context).size.height * 0.1,
+                        child: Column(
+                          children: [
+                            Text(
+                              "Event Date",
+                              textAlign: TextAlign.left,
+                              style: TextStyle(
+                                fontSize:
+                                    MediaQuery.of(context).size.height * 0.03,
+                                fontWeight: FontWeight.bold,
+                              ),
                             ),
-                          ),
-                          Text(
-                            snapshot.data!.year.toString(),
-                            textAlign: TextAlign.right,
-                            style: TextStyle(
-                              fontSize:
-                                  MediaQuery.of(context).size.height * 0.03,
-                              fontWeight: FontWeight.bold,
+                            Text(
+                              snapshot.data!.organizer,
+                              textAlign: TextAlign.right,
+                              style: TextStyle(
+                                fontSize:
+                                    MediaQuery.of(context).size.height * 0.02,
+                                fontWeight: FontWeight.bold,
+                              ),
                             ),
-                          ),
-                        ],
+                          ],
+                        ),
                       ),
-                    ),
-                    Container(
-                      height: MediaQuery.of(context).size.height * 0.1,
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          Text(
-                            "Event URL",
-                            textAlign: TextAlign.left,
-                            style: TextStyle(
-                              fontSize:
-                                  MediaQuery.of(context).size.height * 0.03,
-                              fontWeight: FontWeight.bold,
+                      Container(
+                        height: MediaQuery.of(context).size.height * 0.1,
+                        child: Column(
+                          children: [
+                            Text(
+                              "Event Year",
+                              textAlign: TextAlign.left,
+                              style: TextStyle(
+                                fontSize:
+                                    MediaQuery.of(context).size.height * 0.03,
+                                fontWeight: FontWeight.bold,
+                              ),
                             ),
-                          ),
-                          Text(
-                            snapshot.data!.url,
-                            textAlign: TextAlign.right,
-                            style: TextStyle(
-                              fontSize:
-                                  MediaQuery.of(context).size.height * 0.03,
-                              fontWeight: FontWeight.bold,
+                            Text(
+                              snapshot.data!.year.toString(),
+                              textAlign: TextAlign.right,
+                              style: TextStyle(
+                                fontSize:
+                                    MediaQuery.of(context).size.height * 0.02,
+                                fontWeight: FontWeight.bold,
+                              ),
                             ),
-                          ),
-                        ],
+                          ],
+                        ),
                       ),
-                    ),
-                    Container(
-                      height: MediaQuery.of(context).size.height * 0.1,
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          Text(
-                            "Event Divisions",
-                            textAlign: TextAlign.left,
-                            style: TextStyle(
-                              fontSize:
-                                  MediaQuery.of(context).size.height * 0.03,
-                              fontWeight: FontWeight.bold,
+                      Container(
+                        height: MediaQuery.of(context).size.height * 0.1,
+                        child: Column(
+                          children: [
+                            Text(
+                              "Event URL",
+                              textAlign: TextAlign.left,
+                              style: TextStyle(
+                                fontSize:
+                                    MediaQuery.of(context).size.height * 0.03,
+                                fontWeight: FontWeight.bold,
+                              ),
                             ),
-                          ),
-                          Text(
-                            "later",
-                            textAlign: TextAlign.right,
-                            style: TextStyle(
-                              fontSize:
-                                  MediaQuery.of(context).size.height * 0.03,
-                              fontWeight: FontWeight.bold,
+                            Text(
+                              snapshot.data!.url,
+                              textAlign: TextAlign.right,
+                              style: TextStyle(
+                                fontSize:
+                                    MediaQuery.of(context).size.height * 0.02,
+                                fontWeight: FontWeight.bold,
+                              ),
                             ),
-                          ),
-                        ],
+                          ],
+                        ),
                       ),
-                    ),
-                  ]);
-                } else if (snapshot.hasError) {
-                  return Text("${snapshot.error}");
-                }
-                return CircularProgressIndicator();
-              },
+                      Container(
+                        height: MediaQuery.of(context).size.height * 0.1,
+                        child: Column(
+                          children: [
+                            Text(
+                              "Event Divisions",
+                              textAlign: TextAlign.left,
+                              style: TextStyle(
+                                fontSize:
+                                    MediaQuery.of(context).size.height * 0.03,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                            Text(
+                              "later",
+                              textAlign: TextAlign.right,
+                              style: TextStyle(
+                                fontSize:
+                                    MediaQuery.of(context).size.height * 0.02,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ]);
+                  } else if (snapshot.hasError) {
+                    return Text("${snapshot.error}");
+                  }
+                  return CircularProgressIndicator();
+                },
+              ),
             ),
           ),
         ],
