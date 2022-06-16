a13eaa59c21f75a6c95ca2c0756248612f84df27
sergiusgeoffrey <73834938+sergiusgeoffrey@users.noreply.github.com>
2022-06-17 01:10:38 +0700

    Update student_detail.dart
    fixed without  UI yet

--- pantra_project/lib/pages/student_detail.dart
+++ pantra_project/lib/pages/student_detail.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:pantra_project/models/student.dart';
 import 'package:pantra_project/models/student_creds.dart';
-import 'package:pantra_project/services/student.dart';
 import 'package:pantra_project/services/student_creds.dart';
 
 class StudentDetail extends StatefulWidget {
@@ -29,6 +27,7 @@ class _StudentDetailState extends State<StudentDetail> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
+        
         child: Column(children: [
           SizedBox(
             height: MediaQuery.of(context).size.height * 0.05,
@@ -43,50 +42,126 @@ class _StudentDetailState extends State<StudentDetail> {
               ),
             ),
           ),
-          Container(
-            //Foto
+          SizedBox(
+            height: MediaQuery.of(context).size.height * 0.05,
+          ),
+          SizedBox(
             height: MediaQuery.of(context).size.height * 0.8,
-            child: FutureBuilder<List<StudentCreds>>(
-              future: _studentDetails,
-              builder: (context, snapshot) {
-                if (snapshot.hasData) {
-                  return Container(
-                    child: Row(
-                      children: [
-                        Text(
-                          "Nama - NRP:",
-                          style: TextStyle(
+            child: SingleChildScrollView(
+              child: FutureBuilder<List<StudentCreds>>(
+                future: _studentDetails,
+                builder: (context, snapshot) {
+                  if (snapshot.hasData) {
+                    return Expanded(
+                      child: Column(
+                        children: [
+                          SizedBox(
+                            height: MediaQuery.of(context).size.height * 0.6,
+                            width: MediaQuery.of(context).size.width * 0.65,
+                            child: ClipRRect(
+                              borderRadius: BorderRadius.circular(10),
+                              child: Image.network(
+                                snapshot.data![0].photoFilepath,
+                                fit: BoxFit.cover,
+                              ),
+                            ),
+                          ),
+                          SizedBox(
+                            height: MediaQuery.of(context).size.height * 0.02,
+                          ),
+                          //Nama
+                          //NRP
+                          Text(
+                            "Nama - NRP:",
+                            style: TextStyle(
+                                fontSize:
+                                    MediaQuery.of(context).size.height * 0.02,
+                                fontWeight: FontWeight.bold,
+                                color: const Color.fromRGBO(0,0,0, 1)),
+                          ),
+                          Text(
+                            "${snapshot.data![0].name} - ${snapshot.data![0].nrp}",
+                            style:TextStyle(
                               fontSize:
-                                  MediaQuery.of(context).size.height * 0.03,
-                              fontWeight: FontWeight.bold,
-                              color: const Color.fromRGBO(60, 108, 180, 1)),
-                        ),
-                        SizedBox(
-                          width: MediaQuery.of(context).size.width * 0.1,
-                        ),
-                        Text(
-                          snapshot.data![0].name +
-                              " - " +
-                              snapshot.data![0].nrp,
-                        ),
-                      ],
-                    ),
-                  );
-                } else if (snapshot.hasError) {
-                  return Text("${snapshot.error}");
-                } else {
-                  return const Center(
-                    child: CircularProgressIndicator(),
-                  );
-                }
-              },
+                                    MediaQuery.of(context).size.height * 0.02,
+                                fontWeight: FontWeight.bold,
+                                color: const Color.fromRGBO(60, 108, 180, 1))
+                          ),
+                          SizedBox(
+                            height: MediaQuery.of(context).size.height * 0.02,
+                          ),
+                          //Jurusan
+                          Text(
+                            "Jurusan:",
+                            style: TextStyle(
+                                fontSize:
+                                    MediaQuery.of(context).size.height * 0.02,
+                                fontWeight: FontWeight.bold,
+                                color: const Color.fromRGBO(0,0,0, 1)),
+                          ),
+                          Text(
+                            snapshot.data![0].jurusan,
+                            style:TextStyle(
+                              fontSize:
+                                    MediaQuery.of(context).size.height * 0.02,
+                                fontWeight: FontWeight.bold,
+                                color: const Color.fromRGBO(60, 108, 180, 1))
+                          ),
+                          SizedBox(
+                            height: MediaQuery.of(context).size.height * 0.02,
+                          ),
+                           //Angkatan
+                          Text(
+                            "Angkatan:",
+                            style: TextStyle(
+                                fontSize:
+                                    MediaQuery.of(context).size.height * 0.02,
+                                fontWeight: FontWeight.bold,
+                                color: const Color.fromRGBO(0,0,0, 1)),
+                          ),
+                          Text(
+                            snapshot.data![0].angkatan.toString(),
+                            style:TextStyle(
+                              fontSize:
+                                    MediaQuery.of(context).size.height * 0.02,
+                                fontWeight: FontWeight.bold,
+                                color: const Color.fromRGBO(60, 108, 180, 1))
+                          ),
+                          SizedBox(
+                            height: MediaQuery.of(context).size.height * 0.02,
+                          ),
+                          //Porto
+                          Text(
+                            "Portofolio:",
+                            style: TextStyle(
+                                fontSize:
+                                    MediaQuery.of(context).size.height * 0.02,
+                                fontWeight: FontWeight.bold,
+                                color: const Color.fromRGBO(0,0,0, 1)),
+                          ),
+                          Text(
+                            "TODO NEXT",
+                            style:TextStyle(
+                              fontSize:
+                                    MediaQuery.of(context).size.height * 0.02,
+                                fontWeight: FontWeight.bold,
+                                color: const Color.fromRGBO(60, 108, 180, 1))
+                          ),
+                          //Pengalaman card/popup
+                        ],
+                      ),
+                    );
+                  } else if (snapshot.hasError) {
+                    return Text("${snapshot.error}");
+                  } else {
+                    return const Center(
+                      child: CircularProgressIndicator(),
+                    );
+                  }
+                },
+              ),
             ),
           ),
-          //Nama
-          //NRP
-          //Jurusan Angkatan
-          //Porto
-          //Pengalaman card/popup
           SizedBox(
             height: MediaQuery.of(context).size.height * 0.01,
           ),
